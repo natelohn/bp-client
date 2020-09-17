24243ca5d679e2729ebde904dbaff02d89fd6a04
Nate Lohn <lohndev@gmail.com>
2020-09-16 17:19:49 -0700

    Launch Screen shown w/ react navigation 5+

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LaunchScreen from './src/screens/LaunchScreen'
-import HomeScreen from "./src/screens/HomeScreen";
-import EasyModeScreen from "./src/screens/EasyModeScreen";
-import HardModeScreen from "./src/screens/HardModeScreen";
+// import HomeScreen from "./src/screens/HomeScreen";
+// import EasyModeScreen from "./src/screens/EasyModeScreen";
+// import HardModeScreen from "./src/screens/HardModeScreen";
 
 
-const navigator = createStackNavigator(
-  {
-      Launch: LaunchScreen,
-      Home: HomeScreen,
-      EasyMode: EasyModeScreen,
-      HardMode: HardModeScreen
-  },
-  {
-      initialRouteName: "Launch",
-  }
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(navigator);
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Launch" component={LaunchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
