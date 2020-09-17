a71831447d83050f7172dab0899a9e4bd9839332
Nate Lohn <lohndev@gmail.com>
2020-09-16 18:22:58 -0700

    add font loading

--- App.js
+++ App.js
@@ -2,21 +2,38 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LaunchScreen from './src/screens/LaunchScreen'
+import LaunchScreen from './src/screens/LaunchScreen';
 // import HomeScreen from "./src/screens/HomeScreen";
 // import EasyModeScreen from "./src/screens/EasyModeScreen";
 // import HardModeScreen from "./src/screens/HardModeScreen";
+import { AppLoading } from 'expo';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 
 
 const Stack = createStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Launch" component={LaunchScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    let [fontsLoaded] = useFonts({ Inter_900Black });
+    
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return (
+        <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#DBA28D',
+                },
+                headerTintColor: '#5C240F',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontFamily: 'Inter_900Black'
+                },
+            }}>
+            <Stack.Screen name="Launch" component={LaunchScreen} />
+        </Stack.Navigator>
+        </NavigationContainer>
   );
 }
 
