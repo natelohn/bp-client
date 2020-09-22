cf009f0aa55a5cd66a097586e686b3d1bb099381
Nate Lohn <lohndev@gmail.com>
2020-09-22 09:56:15 -0700

    add reducer to handle display logic in the launch screen

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import ButtonView from '../components/ButtonView'
 
 const styles = StyleSheet.create({
     view: {
         justifyContent: 'center',
-        ...StyleSheet.absoluteFillObject,
+        flex: 1
     },
-    login: {
+    subButton: {
         alignSelf: 'center',
         color: '#5C240F',
         fontSize: 24,
@@ -17,16 +17,52 @@ const styles = StyleSheet.create({
     }
 });
 
-
-const signUp = () => {
-    console.log('Sign Up');
-}
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'sub_button':
+            return { ...state,
+                    signingUp: !state.signingUp,
+                    buttonText: state.subButtonText,
+                    subButtonText: state.buttonText };
+        default:
+            return state;
+    }
+};
 
 const LaunchScreen = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        signingUp: true,
+        buttonText: 'Sign Up',
+        subButtonText: 'Login'
+    });
+    const { signingUp, buttonText, subButtonText} = state;
+    
+    const pressSignUp = () => {
+        console.log('Sign Up', signingUp);
+        console.log('---------------------------------');
+        
+    }
+
+    const pressLogIn = () => {
+        console.log('Log In', !signingUp);
+        console.log('---------------------------------');
+    }
+
+    const mainButtonPressed = () => {
+        console.log('Main');
+        if (signingUp) {
+            pressSignUp()
+        } else {
+            pressLogIn()
+        };
+    }
+    
     return (
         <View style={styles.view}>
-            <ButtonView text="Sign Up" onPressCallback={signUp}/>
-            <Text style={styles.login}>Login</Text>
+            <ButtonView text={buttonText} onPressCallback={mainButtonPressed}/>
+            <TouchableOpacity onPress={() => dispatch({type: 'sub_button'})}>
+                <Text style={styles.subButton}>{subButtonText}</Text>
+            </TouchableOpacity>
         </View>
     );
 }
